9824b87bb2611f80a9f59e532030ef022f084c9f
Viraj110 <119493101+Viraj110@users.noreply.github.com>
2022-11-30 16:44:02 +0530

    Add files via upload

---
+++ hungry.py
@@ -0,0 +1,8 @@
+hungry=input(''are you hungry")
+if hungry= ="Y" or hungry= ="y":
+          print("eat Burger")
+          print("eat samosa")
+          print("eat pizza")
+          print("eat franky")
+else:
+       print("do homework")
